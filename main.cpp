025fb4e6ed308c4b1b64a23eee748170b44d46c6
TINYST <qwertyuiopggcc@163.com>
2024-03-04 23:07:11 +0800

    update

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#define ll long long
+#define int long long
+#define pii pair<int, int>
+#define pb push_back
+#define gcd __gcd
+#define f(i,s,e) for(int i=s;i<=e;i++)
+#define ff(i,s,e) for(int i=s;i>=e;i--)
+#define setbits(x) __builtin_popcount(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 100000007
+#define maxn (ll)(2e5+5000)
+#define INF 0x3f3f3f3f
+
+using namespace std;
+//set<int>::iterator it;
+
+signed main(void)
+{
+    ios::sync_with_stdio(false),cin.tie(nullptr);
+    cout<<"Hello World!";
+    
+
+    return 0;
+}
