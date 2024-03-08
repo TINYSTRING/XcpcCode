4e3c8927670ac09594332f89aad00ee3fcf7f4de
TINYST <qwertyuiopggcc@163.com>
2024-03-08 23:29:40 +0800

    update

--- 001.cpp
+++ 001.cpp
@@ -1,6 +1,5 @@
 #include <cstdio>
 
-int main()
-{
+int main() {
     printf("Hello, world!\n");
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
+#include <iostream>
 #define ll long long
 #define int long long
 #define pii pair<int, int>
 #define pb push_back
 #define gcd __gcd
-#define endl '\n'
 #define all(a) a.begin(), a.end()
 #define mem(a, x) memset(a, x, sizeof(a))
 #define f(i, s, e) for (int i = s; i <= e; i++)
@@ -12,72 +12,17 @@
 #define setbits(x) __builtin_popcount(x)
 #define zrobits(x) __builtin_ctzll(x)
 #define mod 100000007
-#define maxn (ll)(5e5 + 5000)
+#define maxn (ll)(2e5 + 5000)
 #define INF 0x3f3f3f3f
 using namespace std;
 // set<int>::iterator it;
-int n;
-map<int, int> mp;
-int a[maxn] = {0};
-int b[maxn] = {0};
-int c[maxn] = {0};
-int lowbit(int x)
-{
-    return x & -x;
-}
-
-void update(int i, int x)
-{
-    while (i <= n)
-    {
-        a[i] += x;
-        i += lowbit(i);
-    }
-    return;
-}
-
-int query(int i)
-{
-    int ans = 0;
-    while (i > 0)
-    {
-        ans += a[i];
-        i -= lowbit(i);
-    }
-    return ans;
-}
-void solve()
-{
-    cin >> n;
-    int ans = 0;
-    f(i, 1, n)
-    {
-        cin >> b[i];
-        c[i] = b[i];
-    }
-    sort(b + 1, b + n + 1, greater<int>());
-    f(i, 1, n)
-    {
-        mp[b[i]] = i;
-    }
-    f(i, 1, n)
-    {
-        ans += query(mp[c[i]]-1);
-        update(mp[c[i]], 1);
-    }
-    cout << ans << endl;
-    mp.clear();
-    return;
-}
 
 signed main(void)
 {
     ios::sync_with_stdio(false), cin.tie(nullptr);
-    int t = 1;
-    while (t--)
-    {
-        solve();
-    }
+    int n, m;
+    cin >> n >> m;
+    cout << gcd(n, m) << endl;
 
     return 0;
 }
